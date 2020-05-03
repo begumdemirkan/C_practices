1c01ffe91cf7942e10f96f28f881d4bac1526a88
begum <begum@192.168.1.103>
2020-05-03 19:17:14 +0300

    if1

---
+++ 1if1/src/if1.c
@@ -0,0 +1,53 @@
+/*
+ ============================================================================
+ Name        : if1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main(void) {
+	int a;
+	a=8;
+
+	if(a==12){
+		printf(" a is equal to:%d" ,a);
+	}
+	else{
+		printf("a is not equal to 10");
+
+	}
+
+	return EXIT_SUCCESS;
+}*/
+
+//kendi denemem
+int main(){
+	int sayi;
+	scanf("%d",&sayi);
+
+	if(sayi==10){
+		printf("%d sayısı 10'a eşittir.",sayi);
+	}
+
+	else{
+		printf("%d sayısı 10'a eşit değildir.",sayi);
+		}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
