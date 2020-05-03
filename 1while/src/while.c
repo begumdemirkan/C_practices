8e26185dc35072e1f1ff04933c0ca77f78cafa0f
begum <begum@192.168.1.103>
2020-05-03 19:21:45 +0300

    while

---
+++ 1while/src/while.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : while.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	/*int i=0;
+	while (i<10){
+		printf("%d ",i);
+		i++;
+	}*/
+
+	//kendi denemem
+
+	int i=0;
+
+	while(i<=25){
+		printf("%d ",i);
+		i++;
+	}
+	return EXIT_SUCCESS;
+}
+
