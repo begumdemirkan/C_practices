249de1cddeef5922c80f432c9760a58a8ddc496a
begum <begum@192.168.1.103>
2020-05-03 19:15:09 +0300

    area2

---
+++ 1area2/src/area2.c
@@ -0,0 +1,40 @@
+/*
+ ============================================================================
+ Name        : area2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int a, b;
+	int area;
+
+    scanf("%d%d",&a,&b);
+
+	area=a*b;
+	printf("%d",area);
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
